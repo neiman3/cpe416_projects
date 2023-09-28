f2c6337c083d0ca258b23c4d35e1e52202e205af
bdehlsen <beckdehlsen@hotmail.com>
2023-09-28 13:59:56 -0700

    Part 4 pseudocode

--- lab1/lab1_part4.c
+++ lab1/lab1_part4.c
@@ -1,21 +1,46 @@
 /*/**
- * @file lab1_part1.c
+ * @file lab1_part4.c
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-09-25
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
- * 
- * For part 1, write a program that gradually fades LED0 on and then off.  
- * Then, it should do the same with LED1 and then the program should repeat.  The fading effect should be smooth and without flicker.
- * 
- * You can make an LED appear partially on by turning the LED on and off in rapid succession.
- * 
+ *
+ *
+ * Create a program which scrolls the number ’416’ on the display that is 
+ * controlled by the tilt of the board.  The program should work along the 2 axes.  
+ * The number should never completely disappear from the screen, but should show at least 1 digit.
+ *
  */
 
 #include "../library/globals.h"
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+
+// calculate starting index of msg using get_accel funcs
+u08 get_msg_position(u08 msg_size) {
+    u08 pos;
+
+    return pos;
+}
+
+int main(void) {
+    char display_text[25];
+    char msg[4] = "416";
+    u08 position = 0;   // starting index of '416' in display_text
+    init();  //initialize board hardware
+
+    while(1) {
+        position = get_msg_position(4);
+
+        // reset display_text while inserting msg
+
+        print_string(display_text);
+        _delay_ms(25);
+    }
+
+    return 0;
+}
