5ba32ab19475c75bf1702af144fc4b89f5f74ec8
neiman <alhneiman@calpoly.edu>
2023-09-29 12:47:32 -0700

    Done with part 4

--- lab1/lab1_part4.c
+++ lab1/lab1_part4.c
@@ -14,10 +14,14 @@
  *
  */
 
-#include "../library/globals.h"
+#include "./library/globals.h"
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <string.h>
+
+#define SCREEN_SIZE_X 7
+#define SCREEN_SIZE_Y 2
 
 
 // calculate starting index of msg using get_accel funcs
@@ -27,19 +31,45 @@ u08 get_msg_position(u08 msg_size) {
     return pos;
 }
 
+void print_string_wrapping(char *string, u08 base_pos_x, u08 base_pos_y) {
+    u08 pos_x, pos_y;
+    // prints left to right
+    // will wrap around a scrolling text that exceeds the screen size
+    clear_screen();
+    num_chars = strlen(string);
+    for (int i=0; i<num_chars; i++){
+        pos_x = base_pos_x + i;
+        pos_y = base_pos_y;
+        //each character
+        while (pos_x >= SCREEN_SIZE_X) {
+            pos_x = pos_x - SCREEN_SIZE_X;
+        }
+        while (pos_y >= SCREEN_SIZE_Y) {
+            pos_y = pos_y - SCREEN_SIZE_Y;
+        }
+        set_cursor(pos_x, pos_y);
+        print_string(string[i]);
+    }
+    return;
+}
+
 int main(void) {
+    u08 pos_x = 0; //  position x value
+    u08 pos_y = 0; //  position y value
+
     char display_text[25];
     char msg[4] = "416";
     u08 position = 0;   // starting index of '416' in display_text
     init();  //initialize board hardware
+    print_string_wrapping("Hello", 0, 0);
 
     while(1) {
-        position = get_msg_position(4);
+        // position = get_msg_position(4);
 
-        // reset display_text while inserting msg
+        // // reset display_text while inserting msg
 
-        print_string(display_text);
-        _delay_ms(25);
+        // print_string(display_text);
+        // _delay_ms(25);
     }
 
     return 0;
--- main.c
+++ main.c
@@ -1,196 +1,139 @@
 /*/**
- * 
-
-Name:  Alex Neiman and Beck Dehlsen
-Lab 1 part 3
-
-Description: Pong game with LED blinking
-
+ * @file lab1_part4.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-25
+ *
+ * @copyright Copyright (c) 2023
+ *
+ *
+ * Create a program which scrolls the number ’416’ on the display that is 
+ * controlled by the tilt of the board.  The program should work along the 2 axes.  
+ * The number should never completely disappear from the screen, but should show at least 1 digit.
+ *
  */
 
-#include "../library/globals.h"
+#include "./library/globals.h"
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <string.h>
 
-#include "helpers.c"
+#define SCREEN_SIZE_X 7
+#define SCREEN_SIZE_Y 2
 
+#define DELAY_TIME_BASE 1000
+#define DELAY_TIME_MULTIPLIER 15 // ms per bit (accel output)
+#define DEAD_ZONE 3 // multiply by 2 to get total dead zone
+#define ACCEL_CENTER 128
 
-#define PIN_BUTTON DIGITAL5_PIN
-#define BUTTON_SPAM_REJECT_TIME 50 // time, in ms, when a repeat button press will be ignored
-/*
-    read button to pick string, loop thru chars in string with delay
-*/
-
-u08 check_direction(u08 current_position, int8_t current_direction) {
-    // Check position and direction
-    // If it returns 1, it's a valid condition for the user to switch directions
-    // if it returns zero, fon't change direction
-    switch (current_direction)
-    {
-    case 1: // traveling left
-        if (current_position == 4) {
-            return 1;
+void print_string_wrapping(char *string, u08 base_pos_x, u08 base_pos_y) {
+    u08 pos_x = 0;
+    u08 pos_y = 0;
+    // prints left to right
+    // will wrap around a scrolling text that exceeds the screen size
+    clear_screen();
+    u08 num_chars = strlen(string);
+    for (int i=0; i<num_chars; i++){
+        pos_x = base_pos_x + i;
+        pos_y = base_pos_y;
+        //each character
+        while (pos_x >= SCREEN_SIZE_X) {
+            pos_x = pos_x - SCREEN_SIZE_X;
         }
-    case -1:
-        if (current_position == 0) {
-            return 1;
+        while (pos_y >= SCREEN_SIZE_Y) {
+            pos_y = pos_y - SCREEN_SIZE_Y;
         }
-    
-    default:
-        break;
+        lcd_cursor(pos_x, pos_y);
+        print_string(string+i);
     }
-    return 0;
-}
-
-void display_position(u08 led) {
-    // TODO: Simplify IO writes so that it only writes previous IO and current IO instead of writing to all
-    // 0-4 will turn on JUST that IO pin and turn off the other ones
-    // 5 will turn them all off
-    // 6 will turn them all on
-    u08 led_override=0;
-    if (led==6) {
-        led_override = 1;
-    }
-    for (u08 pin=0; pin<5; pin++) {
-        // turn on the LED pin if the current pin is equal to the serlected display position
-        // or, if they all should be on, turn on every LED
-        digital_out(pin, ((pin==led) || led_override));
-    }
-    return;
-}
-
-void init_io() {
-    // 0-4 set up as output
-    // pin 5 set up as button input
-    for (u08 pin=0; pin<5; pin++) {
-        // turn on the LED pin if the current pin is equal to the serlected display position
-        // or, if they all should be on, turn on every LED
-        digital_dir(pin, 1); // 1 mode output
-    }
-    digital_dir(PIN_BUTTON, 0); // button pin input
     return;
 }
 
 int main(void) {
-    // Init
-    init();
-    init_io();
-    // io configuration- input for button, output for leds
+    u08 pos_x = 0; //  position x value
+    u08 pos_y = 0; //  position y value
+    u08 accel_read;
+    int8_t dir_x;
+    int8_t dir_y;
+    u16 timer, update_time_x, update_time_y;
+    u08 update = 0;    
+
+    char display_text[25];
+    char msg[4] = "416";
+
+    timer = 0;
+    update_time_x = 0;
+    update_time_y = 0;
     
+    init();  //initialize board hardware
 
-    // working variables
-    uint8_t button_value;
-    u16 button_last_pressed = 0;
+    print_string_wrapping("Hello", 4, 0);
 
-    u16 timer = 0; //inidialite timer to zero
-    u16 delay_time = 750; // delay time in ms, max 16 bit
-    u16 time_next_led_update = timer + delay_time;
-
-    u08 game_position = 0; // 0 to 4, 0 being the right most
-                            // 0-51 first LED, 52-102, 103-153, 154,-204, 205-255 each directly addressing the output pins D0-D3
-    int8_t game_direction = 1; // 0 going left, -1 going right
-    uint8_t update_flag = 0;
-    uint8_t button_press_flag = 0;
-
-    lcd_cursor(0,0);
-    display_position(6); // all led on
-    print_string("Ready...");
-    delay_ms(1000);
-    lcd_cursor(0,1);
-    display_position(5); // all led off
-    print_string("Set...");
-    delay_ms(1000);
-    clear_screen();
-    lcd_cursor(0,0);
-    print_string("Go!");
-
-    display_position(game_position);
-
-
-    while(1) { // MAIN LOOP- each pass takes 1 ms aka 1khz update rate
-
-        timer++; // increment the timer
-
-        button_value = !digital(PIN_BUTTON); // Remember that since button is pulled high, it's active low...so zero means pressed
-        // We invert that value so that a button_value of 1 means button is pressed
-        if (button_value) {
-            // button was pressed
-            if ((!button_press_flag) && ((button_last_pressed + BUTTON_SPAM_REJECT_TIME) < timer)) {
-                // All button press event stuff go here
-                button_press_flag = 1;
-                
-                button_last_pressed = timer;
-
-                if (check_direction(game_position, game_direction)) {
-                // if the LED is at one of the extents, set the direction to opposite (invert directrion)
-                    game_direction = game_direction * -1;
-                    update_flag = 1;
-                    delay_time = (u16)(delay_time - (delay_time * 5 / 100));
-                } else {
-                    delay_time = (u16)(delay_time - (delay_time * 2 / 100));
-                }
-
-            }
-        } else if (button_press_flag) {
-            // user has let go of the button
-            button_press_flag = 0;
-        }
-        
-        /*if (update_flag) {
-            // do all grapics update here
-            // slice the string and display
-            // get string from user name variables
-            lcd_cursor(0,0);
-            // Write to the new IO pins (button on)
-            // Turn off other IO pins
-        }*/
-        
-        if((timer >= time_next_led_update) || update_flag) {
-
-            // determine the new LED positoin
-            // display it
-
-            // update timer
-            time_next_led_update = timer + delay_time;
-            // reset update flag
-            update_flag=0;
-
-            // increment the position
-            // LED position update here
-            game_position = game_position + game_direction;
-
-            // check to see if the player missed the button press
-            if (game_position > 4) {
-                // Player lost
-                break;
-            }
-
-            
-
-            display_position(game_position);
-        }
-        
-        _delay_ms(1);
-    }
-
-    // end game state
-    // Display 
-    // all LED off
-    clear_screen();
-    lcd_cursor(0,0);
-    print_string("You lose");
-    lcd_cursor(0,1);
-    print_string("t: ");
-    print_num(delay_time);
-    game_position = game_position - game_direction; // undo last direction change
     while(1) {
-        display_position(game_position);
-        delay_ms(400);
-        display_position(5);
-        delay_ms(400);
+        //DEBUG
+
+        // make the math easy- add constant offset 128 to accel data so that 128 = 0
+        // magnitude of acceleration is the absolute value |128 - accel|
+        // That of course means greater magnitude = shorter delay time
+        if (timer > update_time_x) {
+            // read accel yes it's y axis for x
+            accel_read = 128 - get_accel_y();
+            // calculate direction
+            if (accel_read < (ACCEL_CENTER - DEAD_ZONE)) {
+                // its pointing in negative direction
+                dir_x = -1;
+            } else if (accel_read > (ACCEL_CENTER + DEAD_ZONE)) 
+            {
+                // pointing in positive
+                dir_x = 1;
+            } else {
+                // it's in the dead zone
+                dir_x = 0;
+            }
+
+            update_time_x = timer + (DELAY_TIME_BASE - DELAY_TIME_MULTIPLIER * dir_x * (accel_read - 128));
+            // calculate magnitude and update update time x
+            
+            // update pos x based on direction
+            pos_x = pos_x + dir_x;
+            update = 1;
+
+        }
+        if (timer > update_time_y) {
+            // read accel yes it's y axis for x
+            accel_read = 128 - get_accel_x();
+            // calculate direction
+            if (accel_read < (ACCEL_CENTER - DEAD_ZONE)) {
+                // its pointing in negative direction
+                dir_y = -1;
+            } else if (accel_read > (ACCEL_CENTER + DEAD_ZONE)) 
+            {
+                // pointing in positive
+                dir_y = 1;
+            } else {
+                // it's in the dead zone
+                dir_y = 0;
+            }
+
+            update_time_y = timer + (DELAY_TIME_BASE - DELAY_TIME_MULTIPLIER * dir_x * (accel_read - 128));
+            // calculate magnitude and update update time x
+            
+            // update pos x based on direction
+            pos_y = pos_y + dir_y;
+            update = 1;
+
+        }
+
+        if (update) {
+            update = 0;
+            print_string_wrapping(msg, pos_x, pos_y);
+        }
+
+        _delay_ms(1);
+        timer++;
     }
 
+    return 0;
 }
-
-
