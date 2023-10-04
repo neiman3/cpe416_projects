08c550a8680faacf2f0266ff629a73bb0a050f83
bdehlsen <beckdehlsen@hotmail.com>
2023-10-04 09:30:23 -0700

    part 1 pseudocode

--- lab2/lab2_part1.c
+++ lab2/lab2_part1.c
@@ -15,9 +15,19 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-int main(void) {
-   init();  //initialize board hardware
+// call set_servo to spin the motor: 0 for left, 1 for right
+// speed range: -100 to 100
+void motor(uint8_t num, int8_t speed) {
+    set_servo(0, 127);
+}
 
-   return 0;
+int main(void) {
+
+    init();  //initialize board hardware
+
+    print_string("Speed: 0");
+    motor(0, 0);
+
+    return 0;
 }
 
