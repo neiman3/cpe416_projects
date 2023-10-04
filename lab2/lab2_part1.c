79908f3ed53bc0d48b480e315f4f9ff1fefb9c24
bdehlsen <beckdehlsen@hotmail.com>
2023-10-04 09:48:02 -0700

    Co-authored-by: Alex Neiman <neiman3@users.noreply.github.com>

--- lab2/lab2_part1.c
+++ lab2/lab2_part1.c
@@ -16,17 +16,45 @@
 #include <avr/interrupt.h>
 
 // call set_servo to spin the motor: 0 for left, 1 for right
-// speed range: -100 to 100
+// speed range: -100 to 100 -> 0 to 250
+// speed = (speed + 100) * 250 / 200
 void motor(uint8_t num, int8_t speed) {
-    set_servo(0, 127);
+    int16_t sp = (speed + 100) * 255 / 200;
+    // map -100 to 100 to 0 to 250
+    set_servo(0, sp);
+    return;
 }
+
 
 int main(void) {
 
     init();  //initialize board hardware
-
-    print_string("Speed: 0");
-    motor(0, 0);
+    
+    while (1) {
+        for (int8_t i=0; i<=100; i++) {
+            motor(0,i);
+            motor(1,i);
+            lcd_cursor(0,0);
+            print_string("Speed: ");
+            print_num(i);
+            _delay_ms(100);
+        }
+        for (int8_t i=100; i<=0; i++) {
+            motor(0,i);
+            motor(1,i);
+            _delay_ms(100);
+        }
+        for (int8_t i=0; i<=-100; i++) {
+            motor(0,i);
+            motor(1,i);
+            _delay_ms(100);
+        }
+        for (int8_t i=-100; i<=0; i++) {
+            motor(0,i);
+            motor(1,i);
+            _delay_ms(100);
+        }
+    }
 
     return 0;
 }
