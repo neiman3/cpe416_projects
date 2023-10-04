991d24a19bc59a16222d31ef38ac816e57053f3d
bdehlsen <beckdehlsen@hotmail.com>
2023-10-04 08:54:04 -0700

    Initial lab 2

---
+++ lab2/lab2_part1.c.c
@@ -0,0 +1,23 @@
+/**
+ * 
+ *
+ *   Name:  Alex Neiman and Beck Dehlsen
+ *   CPE 416
+ *   Lab 2 part 1
+ *
+ *   Description: Spin motors back and forth while displaying speed
+ *
+ **/
+
+
+#include "../library/globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int main(void) {
+   init();  //initialize board hardware
+
+   return 0;
+}
+
---
+++ lab2/lab2_part2.c.c
@@ -0,0 +1,23 @@
+/**
+ * 
+ *
+ *   Name:  Alex Neiman and Beck Dehlsen
+ *   CPE 416
+ *   Lab 2 part 2
+ *
+ *   Description: Switch between Braitenburg vehicles 2A and 2B
+ *
+ **/
+
+
+#include "../library/globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int main(void) {
+   init();  //initialize board hardware
+
+   return 0;
+}
+
