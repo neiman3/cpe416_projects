7292b8d01758744404c9a7973812894f11f5fa86
neiman <alhneiman@calpoly.edu>
2023-10-06 10:01:53 -0700

    Change sign of for loop

--- main.c
+++ main.c
@@ -15,13 +15,21 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define SERVO_SPEED_CTL_GAIN 3 // One over the gain (divide by this)
+#define TIMESTEP 20
+
 // call set_servo to spin the motor: 0 for left, 1 for right
 // speed range: -100 to 100 -> 0 to 250
 // speed = (speed + 100) * 250 / 200
+// gain
 void motor(uint8_t num, int8_t speed) {
-    int32_t sp = ((int32_t) speed + 100) * 255 / 200;
-    // map -100 to 100 to 0 to 250
-    set_servo(0, sp);
+    int32_t sp = ((int32_t) speed) / SERVO_SPEED_CTL_GAIN * 255 / 200 + 128;
+    if (num == 1) { // selected first wheel
+        // reverse right wheel
+        set_servo(num, 255 - sp + 1);
+    } else {
+        set_servo(num, sp);
+    }
     return;
 }
 
@@ -31,28 +39,38 @@ int main(void) {
     init();  //initialize board hardware
     
     while (1) {
-        for (int8_t i=0; i<=100; i++) {
+        for (int8_t i=0; i<=50; i++) {
             motor(0,i);
             motor(1,i);
             lcd_cursor(0,0);
-            print_string("Speed:");
+            clear_screen();
+            print_string("s1:");
             print_num(i);
-            _delay_ms(100);
+            _delay_ms(TIMESTEP);
         }
-        for (int8_t i=100; i<=0; i++) {
+        for (int8_t i=50; i>=0; i--) {
             motor(0,i);
             motor(1,i);
-            _delay_ms(100);
+            clear_screen();
+            print_string("s2:");
+            print_num(i);
+            _delay_ms(TIMESTEP);
         }
-        for (int8_t i=0; i<=-100; i++) {
+        for (int8_t i=0; i>=-50; i--) {
             motor(0,i);
             motor(1,i);
-            _delay_ms(100);
+            clear_screen();
+            print_string("s3:");
+            print_num(i);
+            _delay_ms(TIMESTEP);
         }
-        for (int8_t i=-100; i<=0; i++) {
+        for (int8_t i=-50; i<=0; i++) {
             motor(0,i);
             motor(1,i);
-            _delay_ms(100);
+            clear_screen();
+            print_string("s4:");
+            print_num(i);
+            _delay_ms(TIMESTEP);
         }
     }
 
