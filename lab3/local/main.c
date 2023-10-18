2212bb3d249a82207614413589e216a4b4401e7b
neiman <alhneiman@calpoly.edu>
2023-10-18 09:36:44 -0700

    Make new lab3 folder

---
+++ lab3/local/main.c
@@ -0,0 +1,18 @@
+/**
+ * 
+ *
+ *   Name:  Alex Neiman and Beck Dehlsen
+ *   CPE 416
+ *   Lab 3 ANN training
+ *
+ **/
+
+
+#include "./library/globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <math.h>
+
+// local machine only
+#include <stdio.h>
