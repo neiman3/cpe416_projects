ef3190f60a59b735ac11c65f7a1cdd1d4bd29f50
bdehlsen <beckdehlsen@hotmail.com>
2023-10-20 08:49:02 -0700

    Start l3p2

--- lab3/lab3_part2.c
+++ lab3/lab3_part2.c
@@ -3,7 +3,7 @@
  *
  *   Name:  Alex Neiman and Beck Dehlsen
  *   CPE 416
- *   Lab 3 Part 2: ANN training
+ *   Lab 3 Part 2: Neural network line following
  *
  **/
 
@@ -12,30 +12,64 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <math.h>
+#include <stdlib.h>
+#include "proportional.h"
+
+#define PIN_SENSOR_L    3
+#define PIN_SENSOR_R    4
+#define MOTOR_L         0
+#define MOTOR_R         1
+#define TIMESTEP        100
 
 #define NUM_WEIGHTS = 17    // number of weights in the NN model, including biases
 #define EPOCHS = 1000       // full training cycles
 
-/*** TODO: RECORD DATASET TO TRAIN ON ***/
 // iterate on training data, adjusting weights after each epoch
-void train_nn(float *weights) {
+void train_neural_network(float *weights, float *data) {
+    return;
+}
+
+// scale sensor inputs, then calculate a motor command using neural net
+motor_command compute_neural_network(u08 left, u08 right) {
     return;
 }
 
 int main(void) {
 
-    // float nn_weights[NUM_WEIGHTS];   // Weights from neural net training
+    float nn_weights[NUM_WEIGHTS];   // Weights from neural net training
+    u08 sensor_l, sensor_r;
+
+    init();
+
+    motor(MOTOR_L,0);
+    motor(MOTOR_R,0);
+    clear_screen();
+    lcd_cursor(0,0);
+    print_string("Proportional");
+
+    // proportional control
+    while(!get_btn()) {
+        sensor_l = analog(PIN_SENSOR_L);
+        sensor_r = analog(PIN_SENSOR_R);
+        motor_command mc = compute_proportional(sensor_l, sensor_r);
+        motor(MOTOR_L, mc.left);
+        motor(MOTOR_R, mc.right);
+        _delay_ms(TIMESTEP);
+    }
+
+    clear_screen();
+    lcd_cursor(0,0);
+    print_string("Data");
 
     motor_command mc = compute_proportional(193, 182);
     
      // init weights pre-training
     for(int i=0; i<NUM_WEIGHTS; i++) {
-        nn_weights = 0;
-        nn_weights = -1;    // bias "weights"
+        nn_weights = rand() / RAND_MAX;
     }
 
     // train model
-    // train_nn(nn_weights);
+    train_neural_network(nn_weights);
 
     printf("%d", mc.left); printf(" ");
     printf("%d", mc.right);
