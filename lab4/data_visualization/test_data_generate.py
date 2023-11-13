73cade150275744ce1a4b2b113be9e89b237039e
neiman <alhneiman@calpoly.edu>
2023-11-13 11:29:55 -0800

    Fix mean std functipon

--- lab4/data_visualization/test_data_generate.py
+++ lab4/data_visualization/test_data_generate.py
@@ -1,6 +1,6 @@
 # generate angles in C struct format
-start = 87
-stop = 91
+start = 0
+stop = 270
 num = 10
 
 l = ["{" + str(int(x * (1 / num * (stop - start) / 360 * 50000) + start / 360 * 50000)) + ",1}" for x in range(0, num)]
